d0541f1ae7beb8dd3ebf014dbdc6488a4ab74f39
Amanda-Shitara <69778375+Amanda-Shitara@users.noreply.github.com>
2022-06-08 03:49:19 -0300

    Update and rename comprar.js to compra.js

--- comprar.js
+++
@@ -1,7 +0,0 @@
-function AdicionarCarrinho(ingresso, qtd, valor, posicao){
-    localStorage.setItem("ingresso" + posicao, ingresso);
-    localStorage.setItem("qtd" + posicao, qtd);
-    valor = valor * qtd;
-    localStorage.setItem("valor" + posicao, valor);
-    alert("Ingresso(s) adicionados ao carrinho com sucesso!");
-}
